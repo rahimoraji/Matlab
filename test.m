5424b5ea1111ea7620e17eee7bdea5b91a54f96d
rahimoraji <rahim_oraji@yahoo.com>
2016-10-06 10:27:29 -0600

    Update test.m
    Small changes were applied

--- test.m
+++ test.m
@@ -1,7 +1,7 @@
 clc
 sigma = 8;
-X =-5.0:0.5:5.0
-Y = X.^2 + sigma*randn(1,length(X))
+X =-5.0:0.5:5.0;
+Y = X.^2 + sigma*randn(1,length(X));
 X = X';
 Y = Y';
 Parameters = PolynomialRegression(X,Y,5,1)
